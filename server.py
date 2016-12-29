037c2802805f670e4ad649b5e5af705aae1e04f9
gammelon <tempmailmah@web.de>
2016-12-29 02:41:06 +0100

    program even runs now, yaaaay

--- server.py
+++ server.py
@@ -1,42 +1,50 @@
-import RPi.GPIO as gp 
+#import RPi.GPIO as gp 
 import time
 import socket
+import socketserver as SocketServer
 import sys
 
 class Piylights:
-	_server
-	_livefft
-	class Server:
-		_livefft
-		
-		def __init__(self, livefft):
-			self._livefft = livefft
-	
-	def __init__(self, port):
-		
-		 
-class RequestHandler(SocketServer.BaseRequestHandler):
-	def processData
-	def handle(self):
-		data = self.request.recv(128)
-		self.request.close()
 
-def red(percentage):
+        class Livefft:
+            def call(self):
+                print("livefft called")
+
+        class UDPHandler(SocketServer.BaseRequestHandler):
+
+            def handle(self):
+                self.data = self.request[0].strip()
+                self.server._livefft.call()
+                self.data = str(self.data,'utf-8')
+                if (self.data == "quit"):
+                    print("quits called")
+                    quit()
+                    #todo kill complete program
+                print(str(self.data))
+	
+        def __init__(self, port):
+            self._livefft = self.Livefft()
+            #handler = self.UPDHandler(self._livefft, port)
+            self._server = SocketServer.UDPServer(("localhost", port), self.UDPHandler)
+            self._server._livefft = self._livefft
+            self._server.serve_forever()
+
+		
+
+#def red(percentage):
 	#set red thing
 
 
 
 
-channels = [2, 4, 6, 8, 10, 12]
-gp.setmode(gp.BOARD)
-gp.setup(channels, gp.OUT)
-gp.output(channels,0)
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('localhost', 12345))
 
 
 
 
 
 if __name__ == "__main__":
-	do_something()
+    channels = [2, 4, 6, 8, 10, 12]
+    #gp.setmode(gp.BOARD)
+    #gp.setup(channels, gp.OUT)
+    #gp.output(channels,0)
+    _piylights = Piylights(12345)
