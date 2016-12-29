c62775d2e3e7949331c5f0ed2a5b0c77bc52d9fe
gammelon <tempmailmah@web.de>
2016-12-29 16:37:11 +0100

    server is now threaded, thanks pythondocs example :D

--- server.py
+++ server.py
@@ -3,12 +3,16 @@ import time
 import socket
 import socketserver as SocketServer
 import sys
+import threading
 
 class Piylights:
 
         class Livefft:
             def call(self):
                 print("livefft called")
+
+        class ThreadedUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
+            pass
 
         class UDPHandler(SocketServer.BaseRequestHandler):
 
@@ -24,23 +28,19 @@ class Piylights:
 	
         def __init__(self, port):
             self._livefft = self.Livefft()
-            #handler = self.UPDHandler(self._livefft, port)
-            self._server = SocketServer.UDPServer(("localhost", port), self.UDPHandler)
+            self._server = self.ThreadedUDPServer(("localhost", port), self.UDPHandler)
             self._server._livefft = self._livefft
-            self._server.serve_forever()
+            server_thread = threading.Thread(target=self._server.serve_forever)
+            server_thread.daemon = True
+            server_thread.start()
 
-		
-
-#def red(percentage):
-	#set red thing
-
-
-
-
-
-
-
-
+def client(ip, port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect((ip, port))
+    try:
+        sock.sendall(b"test")
+    finally:
+        sock.close()
 
 if __name__ == "__main__":
     channels = [2, 4, 6, 8, 10, 12]
@@ -48,3 +48,8 @@ if __name__ == "__main__":
     #gp.setup(channels, gp.OUT)
     #gp.output(channels,0)
     _piylights = Piylights(12345)
+    client("localhost", 12345, "heyo")
+    client("localhost", 12345, "heyo")
+    client("localhost", 12345, "heyo")
+    client("localhost", 12345, "heyo")
+    input("mhh")
